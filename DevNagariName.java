654a2f33444de49ab57fe3e688fda1e9516f5fac
Dhanu <140004656+Dhanashree256@users.noreply.github.com>
2025-03-12 18:03:10 +0530

    Assignment

---
+++ DevNagariName.java
@@ -0,0 +1,13 @@
+public class DevNagariName {
+
+   public static void main(String[] args)
+   {
+       char ch1 = '\u0927';
+       char ch2 = '\u0928';
+       String ch3 = "\u0936\u094D\u0930\u0940";
+
+    System.out.println("Name is " +ch1 +ch2 +ch3);
+
+   }
+
+}
