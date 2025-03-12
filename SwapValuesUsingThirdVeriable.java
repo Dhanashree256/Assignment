d03c25f5a7811b153b2a024b662c0a227c3934bc
Dhanu <140004656+Dhanashree256@users.noreply.github.com>
2025-03-12 19:05:01 +0530

    Assignment

---
+++ SwapValuesUsingThirdVeriable.java
@@ -0,0 +1,26 @@
+public class SwapValuesUsingThirdVeriable {
+
+    public static void main (String[] args)
+    {
+
+        int a =12;
+        int b =15;
+        int c;
+
+        c=a; //12
+        a=b; //15
+        b=c; //12
+
+
+        System.out.println("value of b " +b);
+        System.out.println("value of a " +a);
+
+
+
+
+
+
+    }
+
+
+}
