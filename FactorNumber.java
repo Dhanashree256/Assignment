cd3e4e96fb30b846102aae849dcd3a1a8521da9b
Dhanu <140004656+Dhanashree256@users.noreply.github.com>
2025-03-31 18:46:57 +0530

    numberfactors

---
+++ FactorNumber.java
@@ -0,0 +1,20 @@
+public class FactorNumber {
+
+    public static void main(String[] args)
+    {
+
+        int number=24;
+        int i;
+
+        for(i=1 ;i<=number;i++)
+        {
+            if(number%i ==0)
+            {
+                System.out.println("factor output is" +i);
+            }
+
+        }
+
+
+    }
+}
