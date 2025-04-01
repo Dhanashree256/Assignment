c9edb631d4e023acbe688baaa3aa5d6090837604
Dhanu <140004656+Dhanashree256@users.noreply.github.com>
2025-04-01 20:21:46 +0530

    infiniteloopways

---
+++ ThreeWaysInfiniteLoop.java
@@ -0,0 +1,21 @@
+public class ThreeWaysInfiniteLoop {
+
+    public static void main(String[] args)
+    {
+
+        /*while(true)
+        {
+            System.out.println("waiting for user input");
+
+        }*/
+
+       /* for (;;) {
+           System.out.println("This is an infinite loop!");
+        }*/
+
+        do{
+
+            System.out.println("this will run infinite time");
+        }while(true);
+    }
+}
